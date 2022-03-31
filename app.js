c6e2791d9e6190f0bd2d2a90f46fc2c8a2de1803
Herman Chien <Herman@fet.vue>
2022-03-31 13:39:27 +0800

    vue_html_lab4_complete

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const app = Vue.createApp({
     data: function () {
-        return {
-            primaryGoal: "have a happy life!",
-            planLink:"https://trello.com/"
-        }
-    }
+		return {
+			primaryGoal1: "have a <strong>happy life!</strong>",
+			primaryGoal2: "make a lot of <strong>money</strong>",
+			primaryGoal3: "have a <strong>strong mind</strong>",
+			planLink: "https://trello.com/"
+		}
+	},
+	methods: {
+        outputMessage() {
+			const number1 = Math.random()
+			if (number1 < 0.3) {
+				return this.primaryGoal1
+			} else if (number1 < 0.6) {
+				return this.primaryGoal2
+			} else {
+				return this.primaryGoal3
+			}
+		}
+	}
 })
 app.mount("#my-planner")
